310df0390f75c982080e996c4a2e3d22c7a0c7c8
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-27 13:10:02 -0300

    feat: add platzi client and configration of the conection external API

---
+++ src/main/java/dev/java/ecommerce/basketService/client/PlatziStoreClient.java
@@ -0,0 +1,18 @@
+package dev.java.ecommerce.basketService.client;
+
+import dev.java.ecommerce.basketService.client.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "PlatziStoreClient" , url = "${basket.client.plat}")
+public interface PlatziStoreClient {
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+
+    @GetMapping("/products/{id}")
+    PlatziProductResponse getProductById(@PathVariable Long id);
+}
