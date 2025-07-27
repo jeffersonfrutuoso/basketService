287aedbc7899baa68513e75e5e7f1b7c372ba5d8
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-27 13:14:44 -0300

    feat: add methods signatures of the service

--- src/main/java/dev/java/ecommerce/basketService/controller/ProductController.java
+++ src/main/java/dev/java/ecommerce/basketService/controller/ProductController.java
@@ -1,4 +1,34 @@
 package dev.java.ecommerce.basketService.controller;
 
+import dev.java.ecommerce.basketService.client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketService.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlatziProductResponse>> getAll(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<PlatziProductResponse> getAllById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+
 }
